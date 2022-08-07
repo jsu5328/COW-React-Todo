07a7af0c91eb5fcdeb310ae898bdfd4f525fc346
Jang Soyun <soyoon5328@gmail.com>
2022-08-07 19:26:14 +0900

    feat:  화면 레이아웃 구현 : List View

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 function App() {
   return (
+    <div className="html">
+      <Header></Header>
 
-  );
+      <div className="body">
+        <div className="filter">
+          <select name="selectFilter">
+            <option value="viewAll" defaultValue="selected">탈곡기</option>
+            <option value="">다했다!</option>
+            <option value="">해야한다..</option>
+            <option value="">언제시켰냐?</option>
+          </select>
+        </div>
+        <div className="itemContainer">
+          <TodoItem></TodoItem>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
